38a93092a15926308ff371950cd5c13a8b6305a2
Dowdy <admohan2002.gmail.com>
2023-09-03 18:56:22 +0530

    Six

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,6 @@
 import Anbiyams from "./Anbiyams";
 import Events from "./Events";
 import Footer from "./Footer";
-import Header from "./Header";
 import Priests from "./Priests";
 import Quotes from "./Quotes";
 import Slider from "./Slider";
